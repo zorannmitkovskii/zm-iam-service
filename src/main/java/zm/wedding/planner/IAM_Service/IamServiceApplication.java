b7b88d0f37d27b9e0a6bd8ce023fc82ef3a9a4ac
zoranmitkovski <z.mitkovski@heartforhealth.com>
2025-03-03 20:54:05 +0100

    Initial commit

---
+++ src/main/java/zm/wedding/planner/IAM_Service/IamServiceApplication.java
@@ -0,0 +1,13 @@
+package zm.wedding.planner.IAM_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zm/wedding/planner/IAM_Service/IamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zm.wedding.planner.IAM_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
